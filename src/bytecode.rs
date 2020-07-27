226c0eef187d2f1577ed1b3526b1d473e9b56d06
Eidolon <furyhunter600@gmail.com>
2020-07-27 16:45:36 -0500

    Upgrade to 2018, drop libc, 0.2.0-alpha.0

--- src/bytecode.rs
+++ src/bytecode.rs
@@ -5,48 +5,48 @@ const BITRK: u32 = 1 << 8;
 /// A slot which is either a register (`R`) or constant (`K`).
 #[derive(Clone, Copy, Debug, Eq, PartialEq, Ord, PartialOrd)]
 pub enum RK {
-	/// A register index.
-	R(u8),
-	/// A constant table index.
-	K(u8),
+    /// A register index.
+    R(u8),
+    /// A constant table index.
+    K(u8),
 }
 
 impl RK {
-	/// Convert a number to an `RK`.
-	pub fn decode(value: u32) -> RK {
-		if value & BITRK != 0 {
-			RK::K((value & !BITRK) as u8)
-		} else {
-			RK::R(value as u8)
-		}
-	}
-	/// Convert this `RK` to a number.
-	pub fn encode(&self) -> u32 {
-		match self {
-			&RK::R(r) => r as u32,
-			&RK::K(k) => (k as u32) | BITRK,
-		}
-	}
+    /// Convert a number to an `RK`.
+    pub fn decode(value: u32) -> RK {
+        if value & BITRK != 0 {
+            RK::K((value & !BITRK) as u8)
+        } else {
+            RK::R(value as u8)
+        }
+    }
+    /// Convert this `RK` to a number.
+    pub fn encode(&self) -> u32 {
+        match self {
+            &RK::R(r) => r as u32,
+            &RK::K(k) => (k as u32) | BITRK,
+        }
+    }
 }
 
 /// Encode an instruction with `A`, `B`, and `C` parameters.
 pub fn encode(op: Opcode, a: u8, b: u32, c: u32) -> u32 {
-	(op as u32) | ((a as u32) << 6) | ((c & 0x1ff) << 14) | ((b & 0x1ff) << 23)
+    (op as u32) | ((a as u32) << 6) | ((c & 0x1ff) << 14) | ((b & 0x1ff) << 23)
 }
 
 /// Encode an instruction with `A` and `Bx` parameters.
 pub fn encode_bx(op: Opcode, a: u8, bx: u32) -> u32 {
-	(op as u32) | ((a as u32) << 6) | ((bx & 0x3ffff) << 14)
+    (op as u32) | ((a as u32) << 6) | ((bx & 0x3ffff) << 14)
 }
 
 /// Encode an instruction with `A` and `sBx` parameters.
 pub fn encode_sbx(op: Opcode, a: u8, sbx: i32) -> u32 {
-	(op as u32) | ((a as u32) << 6) | ((((sbx + 0x20000) as u32) & 0x3ffff) << 14)
+    (op as u32) | ((a as u32) << 6) | ((((sbx + 0x20000) as u32) & 0x3ffff) << 14)
 }
 
 /// Encode an instruction with an `Ax` parameter.
 pub fn encode_ax(op: Opcode, ax: u32) -> u32 {
-	(op as u32) | ((ax & 0x3ffffff) << 6)
+    (op as u32) | ((ax & 0x3ffffff) << 6)
 }
 
 // LSB 6      8         9         9  MSB
@@ -60,70 +60,70 @@ pub fn encode_ax(op: Opcode, ax: u32) -> u32 {
 #[repr(u8)]
 #[derive(Clone, Copy, Debug, Eq, PartialEq, Ord, PartialOrd)]
 pub enum Opcode { // Args   Action
-	Move,     // A B    R(A) := R(B)
-	LoadK,    // A Bx   R(A) := Kst(Bx)
-	LoadKX,   // A      R(A) := Kst(extra arg)
-	// ^- the next 'instruction' is always EXTRAARG.
-	LoadBool, // A B C  R(A) := (Bool)B; if (C) pc++
-	LoadNil,  // A B    R(A), R(A+1), ..., R(A+B) := nil
+    Move,     // A B    R(A) := R(B)
+    LoadK,    // A Bx   R(A) := Kst(Bx)
+    LoadKX,   // A      R(A) := Kst(extra arg)
+    // ^- the next 'instruction' is always EXTRAARG.
+    LoadBool, // A B C  R(A) := (Bool)B; if (C) pc++
+    LoadNil,  // A B    R(A), R(A+1), ..., R(A+B) := nil
 
-	GetUpval, // A B    R(A) := UpValue[B]
-	GetTabUp, // A B C  R(A) := UpValue[B][RK(C)]
-	GetTable, // A B C  R(A) := R(B)[RK(C)]
+    GetUpval, // A B    R(A) := UpValue[B]
+    GetTabUp, // A B C  R(A) := UpValue[B][RK(C)]
+    GetTable, // A B C  R(A) := R(B)[RK(C)]
 
-	SetTabUp, // A B C  UpValue[A][RK(B)] := RK(C)
-	SetUpval, // A B    UpValue[B] := R(A)
-	SetTable, // A B C  R(A)[RK(B)] := RK(C)
+    SetTabUp, // A B C  UpValue[A][RK(B)] := RK(C)
+    SetUpval, // A B    UpValue[B] := R(A)
+    SetTable, // A B C  R(A)[RK(B)] := RK(C)
 
-	NewTable, // A B C  R(A) := {} (size = B,C)
+    NewTable, // A B C  R(A) := {} (size = B,C)
 
-	Self_,     // A B C  R(A+1) := R(B); R(A) := R(B)[RK(C)]
+    Self_,     // A B C  R(A+1) := R(B); R(A) := R(B)[RK(C)]
 
-	Add,      // A B C  R(A) := RK(B) + RK(C)
-	Sub,      // A B C  R(A) := RK(B) - RK(C)
-	Mul,      // A B C  R(A) := RK(B) * RK(C)
-	Mod,      // A B C  R(A) := RK(B) % RK(C)
-	Pow,      // A B C  R(A) := RK(B) ^ RK(C)
-	Div,      // A B C  R(A) := RK(B) / RK(C)
-	IntDiv,   // A B C  R(A) := RK(B) // RK(C)
-	BinAnd,   // A B C  R(A) := RK(B) & RK(C)
-	BinOr,    // A B C  R(A) := RK(B) | RK(C)
-	BinXor,   // A B C  R(A) := RK(B) ~ RK(C)
-	ShLeft,   // A B C  R(A) := RK(B) << RK(C)
-	ShRight,  // A B C  R(A) := RK(B) >> RK(C)
-	UnMinus,  // A B    R(A) := -R(B)
-	BinNot,   // A B    R(A) := ~R(B)
-	Not,      // A B    R(A) := not R(B)
-	Len,      // A B    R(A) := length of R(B)
+    Add,      // A B C  R(A) := RK(B) + RK(C)
+    Sub,      // A B C  R(A) := RK(B) - RK(C)
+    Mul,      // A B C  R(A) := RK(B) * RK(C)
+    Mod,      // A B C  R(A) := RK(B) % RK(C)
+    Pow,      // A B C  R(A) := RK(B) ^ RK(C)
+    Div,      // A B C  R(A) := RK(B) / RK(C)
+    IntDiv,   // A B C  R(A) := RK(B) // RK(C)
+    BinAnd,   // A B C  R(A) := RK(B) & RK(C)
+    BinOr,    // A B C  R(A) := RK(B) | RK(C)
+    BinXor,   // A B C  R(A) := RK(B) ~ RK(C)
+    ShLeft,   // A B C  R(A) := RK(B) << RK(C)
+    ShRight,  // A B C  R(A) := RK(B) >> RK(C)
+    UnMinus,  // A B    R(A) := -R(B)
+    BinNot,   // A B    R(A) := ~R(B)
+    Not,      // A B    R(A) := not R(B)
+    Len,      // A B    R(A) := length of R(B)
 
-	Concat,   // A B C  R(A) := R(B).. ... ..R(C)
+    Concat,   // A B C  R(A) := R(B).. ... ..R(C)
 
-	Jump,     // A sBx  pc += sBx; if(A) close all upvalues >= R(A - 1)
-	Eq,       // A B C  if ((RK(B) == RK(C)) ~= A) then pc++
-	Less,     // A B C  if ((RK(B) <  RK(C)) ~= A) then pc++
-	LessEq,   // A B C  if ((RK(B) <= RK(C)) ~= A) then pc++
-	// ^- A specifies what condition the test should accept (true or false).
-	Test,     // A   C 	if not (R(A) <=> C) then pc++
-	TestSet,  // A B C  if (R(B) <=> C) then R(A) := R(B) else pc++
+    Jump,     // A sBx  pc += sBx; if(A) close all upvalues >= R(A - 1)
+    Eq,       // A B C  if ((RK(B) == RK(C)) ~= A) then pc++
+    Less,     // A B C  if ((RK(B) <  RK(C)) ~= A) then pc++
+    LessEq,   // A B C  if ((RK(B) <= RK(C)) ~= A) then pc++
+    // ^- A specifies what condition the test should accept (true or false).
+    Test,     // A   C 	if not (R(A) <=> C) then pc++
+    TestSet,  // A B C  if (R(B) <=> C) then R(A) := R(B) else pc++
 
-	Call,     // A B C  R(A), ... ,R(A+C-2) := R(A)(R(A+1), ... ,R(A+B-1))
-	// ^- if (B == 0) then B = top. If (C == 0), then 'top' is
+    Call,     // A B C  R(A), ... ,R(A+C-2) := R(A)(R(A+1), ... ,R(A+B-1))
+    // ^- if (B == 0) then B = top. If (C == 0), then 'top' is
     //    set to last_result+1, so next open instruction (OP_CALL, OP_RETURN,
-    //    OP_SETLIST) may use 'top'.
-	TailCall, // A B C  return R(A)(R(A+1), ... ,R(A+B-1))
-	Return,   // A B    return R(A), ... ,R(A+B-2)
-	// ^- if (B == 0) then return up to 'top'
+        //    OP_SETLIST) may use 'top'.
+        TailCall, // A B C  return R(A)(R(A+1), ... ,R(A+B-1))
+        Return,   // A B    return R(A), ... ,R(A+B-2)
+        // ^- if (B == 0) then return up to 'top'
 
-	ForLoop,  // A sBx  R(A)+=R(A+2); if R(A) <?= R(A+1) then { pc+=sBx; R(A+3)=R(A) }
-	ForPrep,  // A sBx  R(A)-=R(A+2); pc+=sBx
-	TForCall, // A   C  R(A+3), ... ,R(A+2+C) := R(A)(R(A+1), R(A+2));
-	TForLoop, // A sBx  if R(A+1) ~= nil then { R(A)=R(A+1); pc += sBx }
-	SetList,  // A B C  R(A)[(C-1)*FPF+i] := R(A+i), 1 <= i <= B
-	// ^- if (B == 0) then B = 'top'; if (C == 0) then next
-	//    'instruction' is EXTRAARG(real C).
-	Closure,  // A Bx   R(A) := closure(KPROTO[Bx])
-	VarArg,   // A B    R(A), R(A+1), ..., R(A+B-2) = vararg
-	// ^- if (B == 0) then use actual number of varargs and
-    //    set top (like in OP_CALL with C == 0).
-	ExtraArg, // Ax     extra (larger) argument for previous opcode
-}
+        ForLoop,  // A sBx  R(A)+=R(A+2); if R(A) <?= R(A+1) then { pc+=sBx; R(A+3)=R(A) }
+        ForPrep,  // A sBx  R(A)-=R(A+2); pc+=sBx
+        TForCall, // A   C  R(A+3), ... ,R(A+2+C) := R(A)(R(A+1), R(A+2));
+        TForLoop, // A sBx  if R(A+1) ~= nil then { R(A)=R(A+1); pc += sBx }
+        SetList,  // A B C  R(A)[(C-1)*FPF+i] := R(A+i), 1 <= i <= B
+        // ^- if (B == 0) then B = 'top'; if (C == 0) then next
+        //    'instruction' is EXTRAARG(real C).
+        Closure,  // A Bx   R(A) := closure(KPROTO[Bx])
+        VarArg,   // A B    R(A), R(A+1), ..., R(A+B-2) = vararg
+        // ^- if (B == 0) then use actual number of varargs and
+        //    set top (like in OP_CALL with C == 0).
+        ExtraArg, // Ax     extra (larger) argument for previous opcode
+    }
--- src/read.rs
+++ src/read.rs
@@ -1,141 +1,142 @@
 //! Deserialization code.
 
+use std::error::Error;
 use std::io::{self, Read};
 use std::mem::size_of;
 use byteorder::ReadBytesExt;
 use byteorder::NativeEndian as E;
 
 use super::{
-	SIGNATURE, FORMAT, VERSION, DATA, TEST_INT, TEST_NUMBER,
-	Int, Size, Instruction, Integer, Number,
-	Constant, Upvalue, LocalVar, Debug, Function,
+    SIGNATURE, FORMAT, VERSION, DATA, TEST_INT, TEST_NUMBER,
+    Int, Size, Instruction, Integer, Number,
+    Constant, Upvalue, LocalVar, Debug, Function,
 };
 
 /// Deserialize bytecode into a `Function`.
 pub fn read_file<R: Read>(read: R) -> io::Result<Function> {
-	let mut reader = Reader { out: read };
-	try!(reader.read_header());
-	try!(reader.out.read_u8()); // discard upvals header
-	reader.read_function()
+    let mut reader = Reader { out: read };
+    reader.read_header()?;
+    reader.out.read_u8()?; // discard upvals header
+    reader.read_function()
 }
 
 struct Reader<R: Read> {
-	out: R,
+    out: R,
 }
 
-fn invalid<T, S: Into<Box<::std::error::Error + Send + Sync>>>(s: S) -> io::Result<T> {
-	Err(io::Error::new(io::ErrorKind::InvalidInput, s))
+fn invalid<T, S: Into<Box<dyn Error + Send + Sync>>>(s: S) -> io::Result<T> {
+    Err(io::Error::new(io::ErrorKind::InvalidInput, s))
 }
 
 macro_rules! check {
-	($get:expr, $want:expr, $note:expr) => {{
-		let get = $get;
-		let want = $want;
-		if get != want {
-			return Err(io::Error::new(io::ErrorKind::InvalidInput, format!(
-				"invalid {}, expected {:?} but got {:?}",
-				$note, want, get,
-			)));
-		}
-	}}
+    ($get:expr, $want:expr, $note:expr) => {{
+        let get = $get;
+        let want = $want;
+        if get != want {
+            return Err(io::Error::new(io::ErrorKind::InvalidInput, format!(
+                "invalid {}, expected {:?} but got {:?}",
+                $note, want, get,
+            )));
+        }
+    }}
 }
 
 impl<R: Read> Reader<R> {
-	fn read_all(&mut self, mut buf: &mut [u8]) -> io::Result<()> {
-		let mut start = 0;
-		let len = buf.len();
-		while start < len {
-			let n = try!(self.out.read(&mut buf[start..]));
-			if n == 0 {
-				return invalid("unexpected EOF");
-			}
-			start += n;
-		}
-		Ok(())
-	}
+    fn read_all(&mut self, buf: &mut [u8]) -> io::Result<()> {
+        let mut start = 0;
+        let len = buf.len();
+        while start < len {
+            let n = self.out.read(&mut buf[start..])?;
+            if n == 0 {
+                return invalid("unexpected EOF");
+            }
+            start += n;
+        }
+        Ok(())
+    }
 
-	fn read_header(&mut self) -> io::Result<()> {
-		let mut buffer = [0u8; 6];
-		try!(self.read_all(&mut buffer[..4]));
-		check!(&buffer[..4], SIGNATURE, "signature");
-		check!(try!(self.out.read_u8()), VERSION, "version");
-		check!(try!(self.out.read_u8()), FORMAT, "format");
-		try!(self.read_all(&mut buffer));
-		check!(&buffer, DATA, "test data");
-		check!(try!(self.out.read_u8()), size_of::<Int>() as u8, "sizeof(int)");
-		check!(try!(self.out.read_u8()), size_of::<Size>() as u8, "sizeof(size_t)");
-		check!(try!(self.out.read_u8()), size_of::<Instruction>() as u8, "sizeof(Instruction)");
-		check!(try!(self.out.read_u8()), size_of::<Integer>() as u8, "sizeof(Integer)");
-		check!(try!(self.out.read_u8()), size_of::<Number>() as u8, "sizeof(Number)");
-		check!(try!(self.out.read_i64::<E>()), TEST_INT, "test integer");
-		check!(try!(self.out.read_f64::<E>()), TEST_NUMBER, "test number");
-		Ok(())
-	}
+    fn read_header(&mut self) -> io::Result<()> {
+        let mut buffer = [0u8; 6];
+        self.read_all(&mut buffer[..4])?;
+        check!(&buffer[..4], SIGNATURE, "signature");
+        check!(self.out.read_u8()?, VERSION, "version");
+        check!(self.out.read_u8()?, FORMAT, "format");
+        self.read_all(&mut buffer)?;
+        check!(&buffer, DATA, "test data");
+        check!(self.out.read_u8()?, size_of::<Int>() as u8, "sizeof(int)");
+        check!(self.out.read_u8()?, size_of::<Size>() as u8, "sizeof(size_t)");
+        check!(self.out.read_u8()?, size_of::<Instruction>() as u8, "sizeof(Instruction)");
+        check!(self.out.read_u8()?, size_of::<Integer>() as u8, "sizeof(Integer)");
+        check!(self.out.read_u8()?, size_of::<Number>() as u8, "sizeof(Number)");
+        check!(self.out.read_f64::<E>()?, TEST_NUMBER, "test number");
+        check!(self.out.read_i64::<E>()?, TEST_INT, "test integer");
+        Ok(())
+    }
 
-	fn read_function(&mut self) -> io::Result<Function> {
-		Ok(Function {
-			source: try!(self.read_string()),
-			line_start: try!(self.out.read_i32::<E>()),
-			line_end: try!(self.out.read_i32::<E>()),
-			num_params: try!(self.out.read_u8()),
-			is_vararg: try!(self.out.read_u8()) != 0,
-			max_stack_size: try!(self.out.read_u8()),
-			code: try!(self.read_vec(|this| Ok(try!(this.out.read_u32::<E>())))),
-			constants: try!(self.read_vec(|this| Ok(match try!(this.out.read_u8()) {
-				0x00 => Constant::Nil,
-				0x01 => Constant::Boolean(try!(this.out.read_u8()) != 0),
-				0x03 => Constant::Float(try!(this.out.read_f64::<E>())),
-				0x13 => Constant::Int(try!(this.out.read_i64::<E>())),
-				0x04 => Constant::ShortString(try!(this.read_string())),
-				0x14 => Constant::LongString(try!(this.read_string())),
-				o => return invalid(format!("unknown constant type {}", o)),
-			}))),
-			upvalues: try!(self.read_vec(|this| {
-				let stack = try!(this.out.read_u8());
-				let idx = try!(this.out.read_u8());
-				Ok(match stack {
-					0 => Upvalue::Outer(idx),
-					_ => Upvalue::Stack(idx),
-				})
-			})),
-			protos: try!(self.read_vec(|this| this.read_function())),
-			debug: Debug {
-				lineinfo: try!(self.read_vec(|this| Ok(try!(this.out.read_i32::<E>())))),
-				localvars: try!(self.read_vec(|this| Ok(LocalVar {
-					name: try!(this.read_string()),
-					start_pc: try!(this.out.read_i32::<E>()),
-					end_pc: try!(this.out.read_i32::<E>()),
-				}))),
-				upvalues: try!(self.read_vec(|this| this.read_string())),
-			},
-		})
-	}
+    fn read_function(&mut self) -> io::Result<Function> {
+        Ok(Function {
+            source: self.read_string()?,
+            line_start: self.out.read_i32::<E>()?,
+            line_end: self.out.read_i32::<E>()?,
+            num_params: self.out.read_u8()?,
+            is_vararg: self.out.read_u8()? != 0,
+            max_stack_size: self.out.read_u8()?,
+            code: self.read_vec(|this| Ok(this.out.read_u32::<E>()?))?,
+            constants: self.read_vec(|this| Ok(match this.out.read_u8()? {
+                0x00 => Constant::Nil,
+                0x01 => Constant::Boolean(this.out.read_u8()? != 0),
+                0x03 => Constant::Float(this.out.read_f64::<E>()?),
+                0x13 => Constant::Int(this.out.read_i64::<E>()?),
+                0x04 => Constant::ShortString(this.read_string()?),
+                0x14 => Constant::LongString(this.read_string()?),
+                o => return invalid(format!("unknown constant type {}", o)),
+            }))?,
+            upvalues: self.read_vec(|this| {
+                let stack = this.out.read_u8()?;
+                let idx = this.out.read_u8()?;
+                Ok(match stack {
+                    0 => Upvalue::Outer(idx),
+                    _ => Upvalue::Stack(idx),
+                })
+            })?,
+            protos: self.read_vec(|this| this.read_function())?,
+            debug: Debug {
+                lineinfo: self.read_vec(|this| Ok(this.out.read_i32::<E>()?))?,
+                localvars: self.read_vec(|this| Ok(LocalVar {
+                    name: this.read_string()?,
+                    start_pc: this.out.read_i32::<E>()?,
+                    end_pc: this.out.read_i32::<E>()?,
+                }))?,
+                upvalues: self.read_vec(|this| this.read_string())?,
+            },
+        })
+    }
 
-	#[inline]
-	fn read_vec<F, T>(&mut self, f: F) -> io::Result<Vec<T>>
-		where F: Fn(&mut Self) -> io::Result<T>
-	{
-		let len = try!(self.out.read_u32::<E>());
-		(0..len).map(|_| f(self)).collect()
-	}
+    #[inline]
+    fn read_vec<F, T>(&mut self, f: F) -> io::Result<Vec<T>>
+    where F: Fn(&mut Self) -> io::Result<T>
+    {
+        let len = self.out.read_u32::<E>()?;
+        (0..len).map(|_| f(self)).collect()
+    }
 
-	fn read_string(&mut self) -> io::Result<String> {
-		let first = try!(self.out.read_u8());
-		if first == 0 {
-			Ok(String::new())
-		} else {
-			let len = if first < 0xff {
-				first as usize
-			} else {
-				try!(self.out.read_u32::<E>()) as usize
-			} - 1;
-			let mut buffer = vec![0u8; len];
-			try!(self.read_all(&mut buffer));
-			// TODO: May need to return a Vec<u8> rather than String
-			match String::from_utf8(buffer) {
-				Ok(s) => Ok(s),
-				Err(_) => invalid("not utf8"),
-			}
-		}
-	}
+    fn read_string(&mut self) -> io::Result<String> {
+        let first = self.out.read_u8()?;
+        if first == 0 {
+            Ok(String::new())
+        } else {
+            let len = if first < 0xff {
+                first as usize
+            } else {
+                self.out.read_u32::<E>()? as usize
+            } - 1;
+            let mut buffer = vec![0u8; len];
+            self.read_all(&mut buffer)?;
+            // TODO: May need to return a Vec<u8> rather than String
+            match String::from_utf8(buffer) {
+                Ok(s) => Ok(s),
+                Err(_) => invalid("not utf8"),
+            }
+        }
+    }
 }
